ea09f6cf9e6a80aac556793949e7f476813c0978
IanClayman <Ian.clayman1@gmail.com>
2018-12-05 02:34:02 -0500

    Completed Build

---
+++ Assets/HelpControlsScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpControlsScript : MonoBehaviour {
+
+    public GameObject helpBackground;
+    public GameObject closeHelpButton;
+
+    public GameObject[] helpPages;
+
+    int currPageIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        helpBackground.SetActive(false);
+        closeHelpButton.SetActive(false);
+
+        foreach (GameObject page in helpPages)
+        {
+            page.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OpenHelpPages()
+    {
+        helpBackground.SetActive(true);
+        closeHelpButton.SetActive(true);
+
+        currPageIndex = 0;
+
+        helpPages[currPageIndex].SetActive(true);
+    }
+
+    public void IncrementIndex()
+    {
+        currPageIndex++;
+
+        foreach (GameObject page in helpPages)
+        {
+            page.SetActive(false);
+        }
+
+        helpPages[currPageIndex].SetActive(true);
+    }
+
+    public void DecrementIndex()
+    {
+        currPageIndex--;
+
+        foreach (GameObject page in helpPages)
+        {
+            page.SetActive(false);
+        }
+
+        helpPages[currPageIndex].SetActive(true);
+    }
+
+    public void CloseHelpPages()
+    {
+        foreach (GameObject page in helpPages)
+        {
+            page.SetActive(false);
+        }
+
+        helpBackground.SetActive(false);
+        closeHelpButton.SetActive(false);
+    }
+}
